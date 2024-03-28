3ca0a100509b6f8ae03c2bc32635014ebf00edbf
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-28 12:43:28 +0300

    debugging

--- calc.dart
+++ calc.dart
@@ -2,7 +2,7 @@ import "dart:io";
 import "dart:math";
 
 double numberInput(){
-    print("Enter the first number: ");
+    print("Enter your number: ");
     double num = double.parse(stdin.readLineSync()!);
     return num;
 }
