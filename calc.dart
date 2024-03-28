4a043cdb568201bff0d6309cbbb4ab6d520ac3fd
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-28 12:39:10 +0300

    working on operands

--- calc.dart
+++ calc.dart
@@ -6,3 +6,33 @@ double numberInput(){
     double num = double.parse(stdin.readLineSync()!);
     return num;
 }
+
+String operandInput() {
+    print("Enter your operation eg '+','-','/','*': ");
+    String operand = stdin.readLineSync()!;
+    return operand;
+}
+
+void main() {
+    double num1 = numberInput();
+    String operand = operandInput();
+    double num2 = numberInput();
+    switch (operand) {
+        case "+":
+            print(num1 + num2);
+            break;
+        case "-":
+            print(num1 - num2);
+            break;
+        case "*":
+            print(num1 * num2);
+            break;
+        case "/":
+            print(num1 / num2);
+            break;
+        default:
+            print("Invalid operation");
+            break;
+    }
+    print("${num1} ${operand} ${num2} = ${result}");
+}
